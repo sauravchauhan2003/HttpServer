e9035bfccc24305323ca6b5246e3675ce1875dfb
Saurav <sauravchauhan652003@gmail.com>
2026-03-18 23:58:05 +0530

    First commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,42 @@
+package org.example;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Main {
+    private static volatile boolean running =true;
+    public static void main(String[] args){
+        try{
+        ServerSocket serverSocket=new ServerSocket(8000);
+        System.out.println("Started listening on port 8000");
+
+            Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+                System.out.println("\nShutting down server...");
+                running = false;
+                try {
+                    if (!serverSocket.isClosed()) {
+                        serverSocket.close(); // ✅ CLOSE PORT
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }));
+        while(running){
+            try {
+                Socket client = serverSocket.accept();
+                System.out.println("Request received from: " + client.getInetAddress());
+                client.close();
+            } catch (IOException e) {
+                if (running) {
+                    System.out.println("Error: " + e.getMessage());
+                }
+            }
+        }
+        }
+        catch (IOException e){
+            System.out.println("Port already in use");
+        }
+
+    }
+}
---
+++ src/main/java/org/example/RequestHandler.java
@@ -0,0 +1,9 @@
+package org.example;
+
+import java.net.Socket;
+
+public class RequestHandler {
+    public static void handleRequest(Socket socket){
+
+    }
+}
